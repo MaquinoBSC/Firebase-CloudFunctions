becbabae6df32fb89762e5d28fe93c718e0aeefb
MaquinoBSC <martinare.ipn@gmail.com>
2021-08-31 23:42:43 -0500

    Implementar una schedule function

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,25 @@
 const functions = require("firebase-functions");
+const admin= require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.onFileChange = functions.storage.object().onFinalize(evt => {
-  console.log(evt);
-  return;
+admin.initializeApp();
+
+const db= admin.firestore();
+
+// exports.onFileChange = functions.storage.object().onFinaize(evt => {
+//   console.log(evt);
+//   return;
+// });
+
+// exports.onFileDelete= functions.storage.object().onDelete(evt => {
+//     console.log(evt);
+//     return
+// });
+
+exports.scheduledFunction = functions.pubsub.schedule('0 22 * * *').onRun( async(context) => {
+  const doc= db.collection('schedule').doc('19950120');
+
+  await doc.update({
+    update: admin.firestore.FieldValue.increment(1),
+    time: admin.firestore.Timestamp.now()
+  });
 });
-
-exports.onFileDelete= functions.storage.object().onDelete(evt => {
-    console.log(evt);
-    return
-})
